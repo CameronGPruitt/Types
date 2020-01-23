bdff800f40994002a3d77f2b6d6367ef5b792d2e
CameronPruitt <47153954+CameronPruitt@users.noreply.github.com>
2020-01-23 10:52:45 -0500

    Initial commit

---
+++ types/types/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Cameron";
+            int grade = 100;
+            char letterGrade = 'A';
+            double costOfTuition = 80.00;
+
+            Console.WriteLine($"{firstName} Is the student, {grade} Is the grade he earned, which is an {letterGrade} and the cost of tuition is {costOfTuition}");
+
+        }
+    }
+}
